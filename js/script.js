249952538dc235db26f2596ee006843b75154e19
maffanakmal <maffanakmal@gmail.com>
2024-03-29 05:35:46 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    window.addEventListener('scroll', function () {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            document.getElementById('navbar').classList.add('scrolled');
+        } else {
+            document.getElementById('navbar').classList.remove('scrolled');
+        }
+    });
+});
